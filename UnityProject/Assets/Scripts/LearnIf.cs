8cfa279af95762ecda0f6acfeb0dac6cfb0e5533
Arron0601 <anyo130525@gmail.com>
2019-12-04 15:55:36 +0800

    1204

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool openDoor;
+    public int score = 100;
+    private void Start()
+    {
+         //語法if(布林值) {陳述式或演算法}
+   //()內的林值為true執行{}
+   if (false)
+	{
+            print("yeyeyeyeyeyeyeye");
+	}
+
+    }
+    private void Update()
+    {
+        if (openDoor)
+        {
+            print("開門~");
+        }
+        else
+        {
+            //if()內的布林值為false執行else{}
+            print("不開~");
+        }
+        if (score = 60)
+        {
+            print("及格");
+        }
+        else if (score>=50)
+        {
+            print("可以補考~");
+        }
+        else if (score>=40)
+        {
+            print("付錢讓你補考");
+        } 
+        else
+        {
+            print("被當拉~");
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,72 @@
+﻿
+using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int A = 10, B = 3;
+    public int C = 10, D = 3,E=1,F=10;
+    public float G = 1.5f ,H=99.9f;
+
+    public float hp = 50;
+    public bool key;
+    public int enemy;
+    private void Start() 
+    {
+        #region 數字運算子
+        print(A + B);
+        print(A - B);
+        print(A * B);
+        print(A / B);
+        print(A % B);
+        
+        //指派右邊運算完再給左邊
+        C = 99 + 1;
+        print(C);
+
+        C = C + 1;//原始寫法
+        //遞增、遞減++,--
+        print(D++);//後置遞增先輸出才運算
+        print(++E);//前置遞增先運算再輸出
+
+        //加法指派 - * / %
+        F = F + 100;
+        F += 100;
+        print(F);
+
+        #endregion
+
+        #region 比較運算子
+        print(G > H);//F
+        print(G < H);//T
+        print(G >= H);//F
+        print(G <= H);//T
+        print(G == H);//F
+        print(G != H);//T
+        #endregion
+
+        #region 邏輯運算子
+        //並且&& 有一個false結果是false
+        print(true && true); //t
+        print(true && false);//f
+        print(false && true);//f
+        print(false && false);//f
+
+        //或者 || 有一個true結果就是true
+        print(true || true); //t
+        print(true || false); //t
+        print(false || true); //t
+        print(false || false); //f
+
+        //相反(顛倒)
+        print(!true);//f
+        print(!false);//t
+        #endregion
+
+    }
+    private void Update()
+    {
+        print("掛囉:" + (hp <= 0));
+        print("過關:" + (key || enemy >= 5));
+    
+    }
+}
